994ca7abbea921aadcd431976e7f55c14797fd16
ktiays <cuansky@gmail.com>
2023-08-28 18:10:41 +0800

    Add `EdgeEffect` and `OverScroller`

---
+++ src/edge_effect.rs
@@ -0,0 +1,29 @@
+static VALUE_THRESHOLD: f32 = 4e-3;
+
+#[derive(Debug)]
+pub struct EdgeEffect {
+    delta: f32,
+    c1: f32,
+    c2: f32,
+    offset: f32,
+}
+
+impl EdgeEffect {
+    pub fn absorb(&mut self, velocity: f32, distance: f32) {
+        self.absorb_with_delta(velocity, distance, 12f32)
+    }
+
+    pub fn absorb_with_delta(&mut self, velocity: f32, distance: f32, delta: f32) {
+        self.delta = delta;
+        self.c1 = distance;
+        self.c2 = velocity + delta * distance;
+    }
+
+    pub fn update(&mut self, elapsed: f32) {
+        self.offset = (self.c1 + self.c2 * elapsed) * libm::expf(-self.delta / elapsed);
+    }
+
+    pub fn is_finished(&self) -> bool {
+        libm::fabsf(self.offset) < VALUE_THRESHOLD
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+pub mod edge_effect;
+pub mod over_scroller;
 pub mod velocity_tracker;
 
 #[cfg(test)]
